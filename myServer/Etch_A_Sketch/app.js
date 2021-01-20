f49c50ff3b4f2d86d70db41cc5cac5a6a6bc6534
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-01-19 21:20:19 -0600

    finally made a 8X8 grid using css-grid

--- myServer/Etch_A_Sketch/app.js
+++ myServer/Etch_A_Sketch/app.js
@@ -1,21 +1,59 @@
 let container = document.getElementById('container');
-let count = 12;
 
-let count2 = 12;
+let square = 64;
 
-for (i = 0; i < count2; i++) {
-    let column = document.createElement("div")
-    column.classList.add("square2")
-    container.appendChild(column)
-    for (i = 0; i < count; i++) {
-        let row = document.createElement("div");
-        row.classList.add("square")
-        column.appendChild(row)
-        row.addEventListener('mouseenter', function () {
-            this.style.backgroundColor = "red";
-        })
-    }
+for (i = 0; i < square; i++) {
+    let e = document.createElement("div")
+    e.classList.add("square")
+    container.appendChild(e)
+    e.addEventListener('mouseenter', function () {
+        this.style.backgroundColor = "gray";
+    })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let container = document.getElementById('container');
+// let count = 12;
+
+// let count2 = 12;
+
+// for (i = 0; i < count2; i++) {
+//     let column = document.createElement("div")
+//     column.classList.add("square2")
+//     container.appendChild(column)
+//     for (i = 0; i < count; i++) {
+//         let row = document.createElement("div");
+//         row.classList.add("square")
+//         column.appendChild(row)
+//         row.addEventListener('mouseenter', function () {
+//             this.style.backgroundColor = "red";
+//         })
+//     }
+// }
 
 
 
