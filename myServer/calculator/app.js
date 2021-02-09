97956a757b953865ba1da19e65cb776aa2a811f4
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-02-08 18:49:05 -0600

    version 1.0 MVP

--- myServer/calculator/app.js
+++ myServer/calculator/app.js
@@ -1,9 +1,16 @@
-let add = (a, b) => a + b;
-let subtract = (a, b) => a - b;
-let multiply = (a, b) => a * b;
-let divide = (a, b) => a / b;
 
 let display = document.getElementById("display");
+
+let clearAll = document.getElementById("clear-all");
+let edit = document.getElementById("edit");
+let sign = document.getElementById("sign");
+let multiBtn = document.getElementById("multi-btn");
+let divideBtn = document.getElementById("divide-btn");
+let minusBtn = document.getElementById("minus-btn");
+let plusBtn = document.getElementById("plus-btn");
+let decimal = document.getElementById("decimal");
+let equalBtn = document.getElementById("equal-btn");
+
 
 let numbers = []
 numbers[0] = document.getElementById("zero");
@@ -20,14 +27,72 @@ numbers[9] = document.getElementById("nine");
 
 numbers.forEach(function (num) {
     num.onclick = function () {
-        let val = num.value;
-        display.innerText = val;
+        display.innerText = display.innerText + num.value;
     }
 })
 
+clearAll.onclick = function () {
+    display.innerText = "";
+}
+
+plusBtn.onclick = function () {
+    display.innerText = display.innerText + "+";
+}
+minusBtn.onclick = function () {
+    display.innerText = display.innerText + "-";
+}
+divideBtn.onclick = function () {
+    display.innerText = display.innerText + "/";
+}
+decimal.onclick = function () {
+    display.innerText = display.innerText + ".";
+}
+multiBtn.onclick = function () {
+    display.innerText = display.innerText + "*";
+}
+
+equalBtn.onclick = function () {
+    display.innerText = eval(display.innerText);
+}
+
+edit.onclick = function () {
+    display.innerText = display.innerText.substring(0, display.innerText.length - 1);
+}
+
+
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
 
 
 
+
+
+
+  // console.log(str);
+    // let str = display.innerText;
+    // let test = display.innerText.split("+", "-")
+    // console.log(test)
+
+// plusBtn.onclick = () => display.innerText = display.innerText + "+";
+        // console.log(typeof display.innerText)
+
+// let test = display.innerText.split("")
+    // .reduce(function (n, m) {
+    //     return Number(n) + Number(m)
+    // })
+    // let result = eval(display.innerText)
+    // display.innerText = `${ display.innerText }`
+    // console.log(test)
+        // console.log(display.innerText)
+        // let val = num.value;
+        // inputDisplay = inputDisplay + val;
+// let inputDisplay = display.innerText;
+
+// plusBtn.onclick = function () {
+
+// }
 
 
 // let numbers = []
